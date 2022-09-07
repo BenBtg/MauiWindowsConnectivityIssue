a4957ec757f62e669561807c740a058c86ff7c37
Conor Breen <conor.breen@civica.co.uk>
2022-09-07 18:08:42 +0100

    Sample connectivity threading issue repro app

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WinUIConnectivityIssue;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ ViewModels/MainPageViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace WinUIConnectivityIssue.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            MainThreadCommand = new Command(() => MainThreadResult = $"{Connectivity.Current.NetworkAccess}");
+            BackgroundThreadCommand = new Command(() =>
+                Task.Run(() =>
+                {
+                    BackgroundThreadResult = $"{Connectivity.Current.NetworkAccess}";
+                })
+            );
+        }
+
+        #region Properties
+
+        private string mainThreadResult;
+        public string MainThreadResult
+        {
+            get
+            {
+                return mainThreadResult;
+            }
+            set
+            {
+                mainThreadResult = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string backgroundThreadResult;
+        public string BackgroundThreadResult
+        {
+            get
+            {
+                return backgroundThreadResult;
+            }
+            set
+            {
+                backgroundThreadResult = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Commands
+
+        public ICommand MainThreadCommand { get; }
+
+        public ICommand BackgroundThreadCommand { get; }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event Action OnChange;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+
+        public void NotifyStateChanged() => OnChange?.Invoke();
+
+        #endregion
+    }
+}
